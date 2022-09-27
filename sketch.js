d38f03baa3d5eaf58e7383ee09d6fe521c140cc0
lilabeckwith <114518322+lilabeckwith@users.noreply.github.com>
2022-09-27 14:21:43 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ function draw() {
   noFill();
   square(0, 200, 200);
   drawNetherWhatIDontKnow();
+  drawCow();
 }
 
 function drawNetherWhatIDontKnow() {
@@ -26,4 +27,58 @@ function drawNetherWhatIDontKnow() {
   
 
   
+}
+
+
+function drawCow() {
+  translate(600, 0);
+  noStroke();
+  fill(0, 0, 0);
+  square(0,75, 25);
+  
+  fill(0, 0, 0);
+  square(175, 75, 25);
+  
+  fill(255, 255, 255); 
+  square(25, 75, 25)
+  
+  fill(255, 255, 255);
+  square(150, 75, 25)
+  
+  fill(211, 211, 211); 
+  square(0, 50, 25);
+  
+  fill(211, 211, 211); 
+  square(25, 50, 25);
+  
+  fill(211, 211, 211); 
+  square(150, 50, 25)
+  
+  fill(211, 211, 211); 
+  square(175, 50, 25);
+  
+  fill(62, 38 ,45); 
+  square(0, 100, 25)
+  
+ fill(62, 38 ,45); 
+ square(0, 125, 25)
+  
+ fill(62, 38 ,45); 
+ square(0, 150, 25)
+  
+ fill(62, 38 ,45); 
+ square(0, 175, 25)
+    
+ fill(62, 38 ,45); 
+ square(175, 100, 25)
+  
+ fill(62, 38 ,45); 
+ square(175, 125, 25)
+  
+ fill(52, 42 ,32); 
+ square(175, 150, 25)
+  
+ fill(62, 38 ,45); 
+ square(175, 175, 25)
+   
 }
